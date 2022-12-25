765485f72fbd4f58f97ec04d32d464ef1f91c7d9
Nazarii <devdragon1129@gmail.com>
2022-12-25 17:14:03 +0800

    deprecated build_query corporate/lib/Api/AccountApi 831 to Query::build

--- corporate/lib/Api/AccountApi.php
+++ corporate/lib/Api/AccountApi.php
@@ -828,7 +828,8 @@ class AccountApi
             $headers
         );
 
-        $query = \GuzzleHttp\Psr7\build_query($queryParams);
+        //$query = \GuzzleHttp\Psr7\build_query($queryParams);
+        $query = Query::build($queryParams);
         return new Request(
             'GET',
             $this->config->getHost() . $resourcePath . ($query ? "?{$query}" : ''),
